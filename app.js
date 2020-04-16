0eba7d85c357928dade3e8ac6ccb2865051bd4d1
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-04-16 11:04:22 +0100

    Added Error handling exceptions

--- app.js
+++ app.js
@@ -22,10 +22,21 @@ var encodedAddress = encodeURIComponent(argv.address);
 request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
-}, (err, response, body) => {
-    console.log(JSON.stringify(response, undefined, 2));
-    console.log(`Address: ${body.results[0].formatted_address}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
-    console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+}, (error, response, body) => {
+
+    if (error){
+        console.log('Unable to connect to Google servers.');
+    
+    } 
+    else if (body.status === 'ZERO_RESULTS'){
+        console.log('Unable to find that address');
+    } 
+    else if (body.status === 'OK')
+    {
+        console.log(JSON.stringify(response, undefined, 2));
+        console.log(`Address: ${body.results[0].formatted_address}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+        console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
+    }
 })
 
