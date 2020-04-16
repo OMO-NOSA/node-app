ce2cf4cdbf927ccdd2f8b5a66eac361f10a1ed14
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-04-16 10:36:30 +0100

    Added command line args and a way to dynamically set address

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const request = require('request');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to get weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+    console.log(argv);
+
+var encodedAddress = encodeURIComponent(argv.address);
+
 
 request({
-    url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20street%20philadelphia',
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (err, response, body) => {
-    console.log(JSON.stringify(body, undefined, 2));
+    console.log(JSON.stringify(response, undefined, 2));
+    console.log(`Address: ${body.results[0].formatted_address}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+    console.log(`Latitude: ${body.results[0].geometry.location.lng}`);
 })
 
