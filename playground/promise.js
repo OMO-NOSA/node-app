94aced1f487bbaddce3a9f915ff8c2b5b83a2272
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-04-21 17:24:37 +0100

    Promises resolved on geocodeAddress function

--- playground/promise.js
+++ playground/promise.js
@@ -11,3 +11,29 @@ somePromise.then((message) => {
     console.log('Error: ', errorMessage);
     console.table('mmmmmmm')
 });
+
+
+
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number'){
+                resolve(a + b);
+            } else{
+                reject('Arguments must be numbers');
+            }
+        }, 7000)
+    });
+}
+
+
+asyncAdd(7, 9)
+    .then((res) => {
+        console.log('Result: ', res);
+        return asyncAdd(res, 45)
+    
+    }).then((res) => {
+        console.log('should be :', res);
+    }).catch((errorMessage) => {
+        console.log(errorMessage);
+    });
