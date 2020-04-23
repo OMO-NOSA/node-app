6011940137f7a828375040abccf20f0088c4de66
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-04-23 03:00:01 +0100

    Testing with expect library on mocha

--- test/test.js
+++ test/test.js
@@ -1,12 +1,14 @@
 const utils = require('../utils/util');
-
+const expect = require('expect');
 
 it('Should add two numbers', () => {
     var res = utils.add(45, 8);
 
-    if (res !== 53) {
-        throw new Error(`Expected 53, but got  ${res}`);
-    }
+    expect(res).toBe(53).toBeA('number');
+
+    // if (res !== 53) {
+    //     throw new Error(`Expected 53, but got  ${res}`);
+    // }
 
     //setImmediate(done);
 });
@@ -15,7 +17,17 @@ it('Should add two numbers', () => {
 it('should multiply two numbers', () => {
     var res = utils.multiply(5, 6);
 
-    if (res !== 30) {
-        throw new Error(`Expected 30, but got ${res}.`);
-    }
+    expect(res).toBe(30).toBeA('number');
+    // if (res !== 30) {
+    //     throw new Error(`Expected 30, but got ${res}.`);
+    // }
+});
+it('should set firstName and lastName', () => {
+    var user = { location: 'Lagos', age: 29 };
+    var res = utils.setName(user, 'Osagie Collins');
+
+    expect(res).toInclude({
+        firstName: 'Osagie',
+        lastName: 'Collins'
+    });
 });
--- utils/util.js
+++ utils/util.js
@@ -1,2 +1,12 @@
 module.exports.add = (a, b) => a + b;
 module.exports.multiply = (a, b) => a * b;
+
+module.exports.setName = (user, fullName) => {
+    var names = fullName.split(' ');
+    user = {
+        firstName: names[0],
+        lastName: names[1]
+    }
+
+    return user;
+}
