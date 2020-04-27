4dec03dc972371c84d2879ca05e36d7fb54e3391
Nosa Omorodion <japhet.omorodion@gmail.com>
2020-04-27 22:33:52 +0100

    Added Async testing

--- test/test.js
+++ test/test.js
@@ -13,6 +13,23 @@ it('Should add two numbers', () => {
     //setImmediate(done);
 });
 
+it('Should  async add two numbers', (done) => {
+    utils.asyncAdd(45, 8, (sum) => {
+
+        expect(sum).toBe(53).toBeA('number');
+
+        done()
+
+    });
+
+
+
+    // if (res !== 53) {
+    //     throw new Error(`Expected 53, but got  ${res}`);
+    // }
+
+    //setImmediate(done);
+});
 
 it('should multiply two numbers', () => {
     var res = utils.multiply(5, 6);
@@ -22,6 +39,15 @@ it('should multiply two numbers', () => {
     //     throw new Error(`Expected 30, but got ${res}.`);
     // }
 });
+
+it('should return async multiple of two numbers', (done) => {
+    utils.asynMultiply(5, 6, (multiply) => {
+        expect(multiply).toBe(30).toBeA('number');
+        done();
+    });
+});
+
+
 it('should set firstName and lastName', () => {
     var user = { location: 'Lagos', age: 29 };
     var res = utils.setName(user, 'Osagie Collins');
--- utils/util.js
+++ utils/util.js
@@ -1,5 +1,19 @@
 module.exports.add = (a, b) => a + b;
+
+module.exports.asyncAdd = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b);
+    }, 1000);
+};
+
+
 module.exports.multiply = (a, b) => a * b;
+
+module.exports.asynMultiply = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a * b);
+    }, 1000);
+};
 
 module.exports.setName = (user, fullName) => {
     var names = fullName.split(' ');
